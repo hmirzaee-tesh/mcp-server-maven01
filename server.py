df7df9fda26ea6549b342b48de4d74ac22e1b1b1
hmirzaee-tesh <hanieh.mirzaee@deliveryhero.com>
2026-02-28 16:21:38 +0100

    mcp-1st-commit

---
+++ server.py
@@ -0,0 +1,12 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("greetings")
+
+@mcp.tool()
+def greetings(name:str) -> str:
+   "send greetings"
+
+   return f"Hello {name}, Have a nice day"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
