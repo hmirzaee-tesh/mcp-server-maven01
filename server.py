d83f77393c8d9ae14157742c53a69f462bca231e
hmirzaee-tesh <hanieh.mirzaee@deliveryhero.com>
2026-02-28 16:48:06 +0100

    mcp port added_2

--- server.py
+++ server.py
@@ -9,4 +9,4 @@ def greetings(name:str) -> str:
    return f"Hello {name}, Have a nice day"
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="0.0.0.0", port="8000")
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
