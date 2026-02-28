b6dfc6088675f18ec390e25f67314650669e7026
hmirzaee-tesh <hanieh.mirzaee@deliveryhero.com>
2026-02-28 16:55:00 +0100

    new file added

---
+++ http_server.py
@@ -0,0 +1,12 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("greetings",host="0.0.0.0", port=8000)
+
+@mcp.tool()
+def greetings(name:str) -> str:
+   "send greetings"
+
+   return f"Hello {name}, Have a nice day"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
